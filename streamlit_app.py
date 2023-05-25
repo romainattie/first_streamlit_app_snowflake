8bfd9a4fda53cf2862922ea3d1e3e48f9a226018
Romain Attié <92431813+romainattie@users.noreply.github.com>
2023-05-25 15:11:32 +0200

    Update streamlit_app.py
    plate name added

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Plate 1')
-streamlit.text('Plate 2')
-streamlit.text('Plate 3')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
