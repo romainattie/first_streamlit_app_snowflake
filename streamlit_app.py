8ccd4fa27f0900e64c9d275e251ea275b14544db
Romain Attié <92431813+romainattie@users.noreply.github.com>
2023-05-25 15:10:29 +0200

    Update streamlit_app.py
    header + first 3 plate added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Plate 1')
+streamlit.text('Plate 2')
+streamlit.text('Plate 3')
